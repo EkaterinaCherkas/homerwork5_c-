89b7c29b05212c7104e5ca4afd53dfa0ee2331a3
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-13 22:57:53 +0300

    add task2 about рандомный массив,найти сум эл стоящих на нечёт позициях

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+Console.Clear();
+int[] MyArray = FillArray(7 , 1 , 100);
+int[] FillArray(int size, int MinValue, int MaxValue){
+    int[] mas = new int[size];
+    for(int i = 0; i<size; i++){
+    mas[i]=new Random().Next(MinValue,MaxValue);
+    Console.WriteLine($"{mas[i]} ");}
+    return mas;
+}
+Console.WriteLine($"Sum = {FindSum(MyArray)}");
+int FindSum(int[] array){
+    int sum = 0;
+    for(int i = 1; i<array.Length;i+=2){
+            sum = array[i] + sum;
+        }
+        return sum;
+    }
+    
